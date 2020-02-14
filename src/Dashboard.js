b96f6109ceb2e059bda61567dfa39e3b049a6aff
unknown <abhishekguptaatweb17@gmail.com>
2020-02-14 20:55:17 +0530

    add all

---
+++ src/Dashboard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Dashboard = () =>
+{
+    return ( 
+        <div>
+        <div>
+        <h1>D</h1>
+        </div>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/Food.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
+import './App.css';
+
+const Food = () => {
+    const APP_ID = "42b10788";
+    const APP_KEY = "a316a943a8444421ad03c292c5b68874";
+
+    const [recipes, setRecipes] = useState([]);
+    const [search, setSearch] = useState("");
+    const [query, setQuery] = useState('chicken');
+
+    useEffect(() => {
+        getRecipes();
+    }, [query]);
+
+    const getRecipes = async () => {
+        const response = await fetch(
+            `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
+        const data = await response.json();
+        setRecipes(data.hits);
+    }
+
+    const updateSearch = e => {
+        setSearch(e.target.value);
+    }
+
+    const getSearch = e => {
+        e.preventDefault();
+        setQuery(search);
+        setSearch('');
+    }
+
+    return (
+        <div className="App">
+            <form onSubmit={getSearch} className="search-form">
+                <input className="search-bar" type="text" value={search} onChange={updateSearch} />
+                <button className="search-button" type="submit">Search</button>
+            </form>
+            <div className="recipes">
+                {
+                    recipes.map(recipe =>
+                        (
+                            <Recipe
+                                key={recipe.recipe.label}
+                                title={recipe.recipe.label}
+                                calories={recipe.recipe.calories}
+                                image={recipe.recipe.image}
+                                ingredients={recipe.recipe.ingredients}
+                            />
+                        ))
+                }
+            </div>
+        </div>
+
+    )
+}
+
+export default Food;
